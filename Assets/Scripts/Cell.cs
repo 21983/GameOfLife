6469eb1006cf21dd8a9a62f7912cafc75aed7aed
Joppe van Basten <21983@ma-web.nl>
2017-11-07 14:55:25 +0100

    First Commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    public bool alive = false;
+    public int aliveNeighbors = 0;
+    private Material mat;
+
+    public bool tempAlive;
+
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    public void SetAlive(bool val)
+    {
+        alive = val;
+        mat.color = alive ? new Color(0, 1, 0) : new Color(1, 0, 0);
+    }
+
+}
